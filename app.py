deb6e98c4580d30e11708815c5f6987cbf399a58
isv-bit <isvdisenointeractivo@gmail.com>
2026-02-26 14:13:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from PIL import Image
 import base64
 
 st.title("RINRIN RENACUAJO")
-image = Image.open('rinrin.png')
+image = Image.open('rinrin.jpg')
 st.image(image, width=350)
 with st.sidebar:
     st.subheader("Escribe y/o selecciona texto para ser escuchado.")
